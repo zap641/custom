5b12dc19ab5cf793defba83b5a3e00f131c82079
zap641 <121319707+zap641@users.noreply.github.com>
2023-11-04 23:43:42 +1000

    Added transparency

--- chadrc.lua
+++ chadrc.lua
@@ -1,6 +1,9 @@
 ---@type ChadrcConfig
 local M = {}
-M.ui = { theme = 'rxyhn' }
+M.ui = {
+    theme = 'rxyhn',
+    transparency = true
+}
 M.plugins = 'custom.plugins'
 M.mappings = require 'custom.mappings'
 return M
